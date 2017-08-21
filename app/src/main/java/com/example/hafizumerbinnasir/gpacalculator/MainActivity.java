d5809a730ce0bfeb179574d10dce00a4cc298fe0
HP <umernasir19@gmail.com>
2017-08-21 08:19:22 -0700

    first commit

--- app/src/main/java/com/example/hafizumerbinnasir/gpacalculator/MainActivity.java
+++ app/src/main/java/com/example/hafizumerbinnasir/gpacalculator/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String i=paper.getText().toString();
-               j=Integer.parseInt(i);
+                j=Integer.parseInt(i);
                 Intent intent=new Intent(MainActivity.this,paper_input.class);
                 startActivity(intent);
 
